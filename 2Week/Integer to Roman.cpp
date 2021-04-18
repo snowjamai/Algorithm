b70b2a72564ae97ebce9ec305559ab07a69cdc60
DESKTOP-TTP4G8B\snowjam1994 <snowjam02@naver.com>
2021-04-18 17:43:45 +0900

    Roman to Integer

--- 2Week/Integer to Roman.cpp
+++ 2Week/Integer to Roman.cpp
@@ -1,67 +1,97 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        int tmp;
-        int cnt;
-        string result;
+        string s;
+        int re;
         
-        while(num){
-            tmp = num / 1000;
-            if(tmp != 0){
-                for(int i = 0; i < tmp; ++i)
-                    result.push_back('M');
-                num -= 1000 * tmp;
-            }
-            else{
-                tmp = num / 500;
-                if(tmp != 0){
-                    for(int i = 0; i < tmp; ++i)
-                        result.push_back('D');
-                    num -= 500 * tmp;
-                }
-                else{
-                    tmp = num / 100;
-                    if(tmp != 0){
-                        for(int i = 0; i < tmp; ++i)
-                            result.push_back('C');
-                        num -= 100 * tmp;
-                    }
-                    else{
-                        tmp = num / 50;
-                        if(tmp != 0){
-                            for(int i = 0; i < tmp; ++i)
-                                result.push_back('L');
-                            num -= 50 * tmp;
-                        }
-                        else{
-                            tmp = num / 10;
-                            if(tmp != 0){
-                                for(int i = 0; i < tmp; ++i)
-                                    result.push_back('X');
-                                num -= 10 * tmp;
-                            }
-                            else{
-                                tmp = num / 5;
-                                if(tmp != 0){
-                                    for(int i = 0; i < tmp; ++i)
-                                        result.push_back('V');
-                                    num -= 5 * tmp;
-                                }
-                                else{
-                                    tmp = num;
-                                    for(int i = 0; i < tmp; ++i)
-                                        result.push_back('I');
-                                    num = 0;
-                                }
-                            
-                            }
-                                
-                        }
-                        
-                    }
-                }
-            }        
+        re = num / 1000;
+        num = num % 1000;
+        if(re){
+            for(int i = 0; i < re; ++i)
+                s.push_back('M');
         }
-        return result;
+        re = num / 100;
+        num = num % 100;
+        
+        if(re>5 && re < 9){
+            s.push_back('D');
+            for(int i = 0; i < re - 5; ++i){
+                s.push_back('C');
+            }
+        }
+        else{
+            switch(re){
+            case 9:
+                s.push_back('C');
+                s.push_back('M');
+                break;
+            case 4:
+                s.push_back('C');
+                s.push_back('D');
+                break;
+            case 5:
+                s.push_back('D');
+                break;
+            default:
+                for(int i =0; i < re; ++i)
+                    s.push_back('C');
+                break;                
+            }
+        }
+        
+        
+        re = num / 10;
+        num = num% 10;
+        
+        if(re > 5 && re < 9){
+            s.push_back('L');
+            for(int i = 0; i < re - 5; ++i)
+                s.push_back('X');
+        }
+        else{
+        switch(re){
+            case 9:
+                s.push_back('X');
+                s.push_back('C');
+                break;
+            case 4:
+                s.push_back('X');
+                s.push_back('L');
+                break;
+            case 5:
+                s.push_back('L');
+                break;
+            default:
+                for(int i =0; i < re; ++i)
+                    s.push_back('X');
+                break;          
+        }
+        }
+        if(num > 5 && num < 9){
+            s.push_back('V');
+            
+            for(int i = 0; i < num -5; ++i)
+                s.push_back('I');
+        }
+        else{
+            switch(num){
+                    case 9:
+                s.push_back('I');
+                s.push_back('X');
+                break;
+            case 4:
+                s.push_back('I');
+                s.push_back('V');
+                break;
+            case 5:
+                s.push_back('V');
+                break;
+            default:
+                for(int i =0; i < num; ++i)
+                    s.push_back('I');
+                break;         
+            }
+        }
+        return s;
     }
 };
