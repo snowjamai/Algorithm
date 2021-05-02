7e212aed5a906f25534aaca09f9f6ed7d6d06e4d
DESKTOP-TTP4G8B\snowjam1994 <snowjam02@naver.com>
2021-05-02 22:06:10 +0900

    algo solve

--- 3Week/DP/DP/2156.cpp
+++ 3Week/DP/DP/2156.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
-int 
-int main() {
+int Num[10001];
+int Max[10001];
 
+int big(int a, int b);
+
+int main() {
+	int N;
+
+	scanf("%d", &N);
+
+	for (int n = 1; n <= N; ++n) {
+		scanf("%d", &Num[n]);
+	}
+	Max[1] = Num[1];
+	Max[2] = Num[2];
+
+	for (int i = 3; i <= N; ++i)
+		Max[i] = big(Max[i - 2], Max[i - 3] + Num[i - 1]) + Num[i];
+
+	printf("%d", big(Max[N], Max[N - 1]));
+
+}
+
+int big(int a, int b) {
+	return a > b ? a : b;
 }
