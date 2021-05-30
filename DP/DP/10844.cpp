55bb11d69c0f6800dd2983084ba5adeeb900946d
DESKTOP-TTP4G8B\snowjam1994 <snowjam02@naver.com>
2021-05-30 20:23:10 +0900

    10844.cpp

--- DP/DP/10844.cpp
+++ DP/DP/10844.cpp
@@ -15,15 +15,13 @@ int main() {
 	DP[2][9] = 1;
 	
 	for (int i = 3; i <= N; ++i) {
-		for (int j = 2; j < 9; ++j) {
-			
-			DP[i][j] = (DP[i - 1][j - 1] + DP[i - 1][j + 1]) % 1000000000;
-			
+		for (int j = 2; j < 9; ++j) {			
+			DP[i][j] = (DP[i - 1][j - 1] + DP[i - 1][j + 1]) % 1000000000;			
 		}
 		DP[i][9] = DP[i - 1][8] % 1000000000;
 		DP[i][1] = (DP[i - 1][2] + DP[i - 2][1]) % 1000000000;
 	}
 	for (int i = 1; i <= 9; ++i)
-		sum = (sum + DP[N][i]) % 1000000000;
-	printf("%ld", sum);
+		sum = (sum + DP[N][i]) ;
+	printf("%ld", sum%100000000000);
 }                                                                                                                                                                                          
